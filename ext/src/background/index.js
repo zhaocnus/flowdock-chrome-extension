0d1e929c54db4ffd8cb80fc99001cea4b060ec44
xiaoyang zhao <zhaocnus@gmail.com>
2017-04-16 00:33:08 -0700

    add logic to scrape thread title

--- ext/src/background/index.js
+++ ext/src/background/index.js
@@ -1,9 +1,28 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
-  console.log(details);
-});
+  let url = new URL(details.url);
 
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if (request.greeting === 'hello')
-//       sendResponse({farewell: 'goodbye'});
-//   });
+  // get thread id from url
+  // Example:
+  // https://www.flowdock.com/app/<company_name>/<flow_name>/threads/fzmGbpx_blEX5ctrjfyvy5EH5_8
+  if (url.origin !== 'https://www.flowdock.com') return;
+
+  let segments = url.pathname.split('/').slice(-3);
+
+  if (segments.length < 3) return;
+
+  if (segments[1] !== 'threads') return;
+
+  let thread = {
+    url: details.url,
+    flow: segments[0],
+    id: segments[2]
+  };
+
+  chrome.tabs.sendMessage(details.tabId, { type: 'SCRAPE_THREAD_DATA' }, response => {
+    thread.text = response;
+
+    console.log(thread);
+  });
+
+
+});
--- ext/src/contentScript/index.js
+++ ext/src/contentScript/index.js
@@ -1,3 +1,19 @@
-// chrome.runtime.sendMessage({greeting: 'hello'}, function(response) {
-//   console.log(response.farewell);
-// });
+function scrapeThreadContent(res) {
+  let threadContentEl = document.querySelector('#thread .thread-content');
+
+  if (!threadContentEl) return res();
+
+  let titleEl = threadContentEl.querySelector('.title-body');
+
+  if (!titleEl) return res();
+
+  res(titleEl.textContent.replace(/\r?\n|\r/, '').trim());
+}
+
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+  switch (req.type) {
+    case 'SCRAPE_THREAD_DATA':
+      scrapeThreadContent(res);
+      break;
+  }
+});
